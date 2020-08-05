1b95c8f58edb16a9244498aedb1e650969c7859f
unknown <veera.github6@gmail.com>
2020-08-05 22:22:10 +0530

    This modified test bash file

--- test.bash
+++ test.bash
@@ -3,4 +3,11 @@
 This is my first shall scripting
 
 echo " This is my first programming for shell scripting"
+echo ".............."
+echo ".............."
+echo ".............."
+echo "here am coding all the git commands"
+echo "............."
+echo "............."
+
 
